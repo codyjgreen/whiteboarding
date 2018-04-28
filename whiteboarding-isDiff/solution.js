74c6b30c615926db3f54876d0494c826f4cc5950
cody <codyjeromegreen@gmail.com>
2018-04-28 10:07:55 -0700

    isDiff whiteboarding

---
+++ whiteboarding-isDiff/solution.js
@@ -0,0 +1,18 @@
+'use strict';
+let obj1 = {'foo': 'bar'};
+let obj2 = {'foo': 'pop'};
+
+function isDiff(obj1, obj2, isTwisted = false){
+
+  for(let key in obj1){
+    if(!obj2.hasOwnProperty(key)){
+      return true;
+    }
+    if(obj1[key] !== obj2[key]){
+      return true;
+    }
+    if (!isTwisted){return isDiff(obj2 ,obj1, isTwisted);
+    }
+    return false;
+  };
+};
